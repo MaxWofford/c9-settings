c5c113e73313a00c67aaa0a41d6539de56ecae5e
Max Wofford <max@hackedu.us>
2015-07-08 14:41:04 -0700

    setup: git folder is removed before livereload is run

--- .setup.sh
+++ .setup.sh
@@ -1,1 +1,2 @@
-curl -sL https://git.io/vtbp6 | sudo dd of=/usr/local/bin/live_reload && sudo chmod +x /usr/local/bin/live_reload && live_reload && rm -rf .git/
+rm -rf .git/
+curl -sL https://git.io/vtbp6 | sudo dd of=/usr/local/bin/live_reload && sudo chmod +x /usr/local/bin/live_reload && live_reload
